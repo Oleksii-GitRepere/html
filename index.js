7ad8157b3ebbeaeca43a4dffc4a2be0d94a7b599
Oleksii-GitRepere <oleksii485th@gmail.com>
2023-02-22 23:13:01 +0200

    Added first img in existing div in htmldoc

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var slide_1 = document.getElementById("img_carousel_div");
+var slide_1 = document.getElementById("slide_1");
 /*
 var slide_2 = document.getElementById("slide_2");
 var slide_3 = document.getElementById("slide_3");
@@ -6,9 +6,9 @@ var slide_4 = document.getElementById("slide_4");
 var slide_5 = document.getElementById("slide_5");
 */
 
-slide_1.innerHTML += `<button name="->" style="font-size: 15px; width: 50px; height: 50px;" class="img_carousel__next">next</button> +
-                        <div style="height: 250px; width: 100%;"><img style="height: 100%; width: 100%" src="../img/desserts/Turkey/cezerye.jpg" alt=""></img></div> + 
-                        <button name="<-" style="font-size: 15px; width: 50px; height: 50px;" class="img_carousel__prev">prev</button>`;
+slide_1.innerHTML += `<div dtyle="height: 320px; width: 100%">
+                        <img style="height: 100%; width: 100%" src="../img/desserts/Turkey/bal kaymak.jpg" alt=""></img>
+                        </div>`;
 /*
 slide_2.innerHTML += `<img style="height: 100%; width: 100%" src="../img/desserts/Turkey/bal kaymak.jpg" alt=""></img>`;
 slide_3.innerHTML += `<img style="height: 100%; width: 100%" src="../img/desserts/Turkey/asure.jpg" alt=""></img>`;
