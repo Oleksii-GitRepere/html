33ba831ec39dfa478da1bf220c937890be0da0a4
Oleksii-GitRepere <oleksii485th@gmail.com>
2023-02-22 23:29:43 +0200

    Add justify-content to img_carousel_div in html

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var slide_1 = document.getElementById("img_carousel_div");
+var carousel_wrapper = document.getElementById("img_carousel_div");
 /*
 var slide_2 = document.getElementById("slide_2");
 var slide_3 = document.getElementById("slide_3");
@@ -6,12 +6,22 @@ var slide_4 = document.getElementById("slide_4");
 var slide_5 = document.getElementById("slide_5");
 */
 
-slide_1.innerHTML += `<div style="height: 320px; width: 100%">
-                        <img style="height: 100%; width: 100%" src="../img/desserts/Turkey/bal kaymak.jpg" alt=""></img>
-                        </div>`;
-/*
-slide_2.innerHTML += `<img style="height: 100%; width: 100%" src="../img/desserts/Turkey/bal kaymak.jpg" alt=""></img>`;
-slide_3.innerHTML += `<img style="height: 100%; width: 100%" src="../img/desserts/Turkey/asure.jpg" alt=""></img>`;
-slide_4.innerHTML += `<img style="height: 100%; width: 100%" src="../img/desserts/Turkey/tulumba.jpg" alt=""></img>`;
-slide_5.innerHTML += `<img style="height: 100%; width: 100%" src="../img/desserts/Turkey/sobiyet.jpg" alt=""></img>`;
+carousel_slides = 
+{
+    [],
+    [],
+    [],
+    []
+}
+    slide_2.innerHTML += `<img style="height: 100%; width: 100%" src="../img/desserts/Turkey/bal kaymak.jpg" alt=""></img>`;
+    slide_3.innerHTML += `<img style="height: 100%; width: 100%" src="../img/desserts/Turkey/asure.jpg" alt=""></img>`;
+    slide_4.innerHTML += `<img style="height: 100%; width: 100%" src="../img/desserts/Turkey/tulumba.jpg" alt=""></img>`;
+    slide_5.innerHTML += `<img style="height: 100%; width: 100%" src="../img/desserts/Turkey/sobiyet.jpg" alt=""></img>`;
 */
+
+carousel_wrapper.innerHTML += `<div style="height: 320px; width: 100%"> 
+                        <img style="height: 100%; width: 100%" src="../img/desserts/Turkey/bal kaymak.jpg" alt=""></img> 
+                        </div>
+                        <div style="height: 320px; width: 100%"> 
+                        <img style="height: 100%; width: 100%" src="../img/desserts/Turkey/asure.jpg" alt=""></img> 
+                        </div>`;
