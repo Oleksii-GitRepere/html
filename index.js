1a9f057a02660aff17973be7d930f28fa0660d6f
Oleksii-GitRepere <oleksii485th@gmail.com>
2023-02-22 22:49:53 +0200

    Try to display imgs from .js file

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ var slide_3 = document.getElementById("slide_3");
 var slide_4 = document.getElementById("slide_4");
 var slide_5 = document.getElementById("slide_5");
 
-slide_1.innerHTML += `<img style="height: 100%; width: 100%" src="img/desserts/Turkey/cezerye.jpg" alt=""></img>`;
-slide_2.innerHTML += `<img style="height: 100%; width: 100%" src="img/desserts/Turkey/bal kaymak.jpg" alt=""></img>`;
-slide_3.innerHTML += `<img style="height: 100%; width: 100%" src="img/desserts/Turkey/asure.jpg" alt=""></img>`;
-slide_4.innerHTML += `<img style="height: 100%; width: 100%" src="img/desserts/Turkey/tulumba.jpg" alt=""></img>`;
-slide_5.innerHTML += `<img style="height: 100%; width: 100%" src="img/desserts/Turkey/sobiyet.jpg" alt=""></img>`;
+slide_1.innerHTML += `<img style="height: 100%; width: 100%" src="../img/desserts/Turkey/cezerye.jpg" alt=""></img>`;
+slide_2.innerHTML += `<img style="height: 100%; width: 100%" src="../img/desserts/Turkey/bal kaymak.jpg" alt=""></img>`;
+slide_3.innerHTML += `<img style="height: 100%; width: 100%" src="../img/desserts/Turkey/asure.jpg" alt=""></img>`;
+slide_4.innerHTML += `<img style="height: 100%; width: 100%" src="../img/desserts/Turkey/tulumba.jpg" alt=""></img>`;
+slide_5.innerHTML += `<img style="height: 100%; width: 100%" src="../img/desserts/Turkey/sobiyet.jpg" alt=""></img>`;
