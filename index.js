ab83fa5d0a24d358a66f6d5587596f7b1971e32e
Oleksii-GitRepere <oleksii485th@gmail.com>
2023-02-24 01:02:24 +0200

    try create proper slides order

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ setInterval(()=>
     }else if(window.screen.availWidth > 950 )
     {
         document.getElementById("useful_p").innerHTML = window.screen.availWidth;
-        carousel_wrapper.innerHTML =  (num+1 > carousel_slides.length-1) ? (carousel_slides[num-1] + carousel_slides[num] + carousel_slides[0]) : (carousel_slides[num-1] + carousel_slides[num] + carousel_slides[num+1]); //+carousel_slides[1];
+        carousel_wrapper.innerHTML =  (num+1 > carousel_slides.length-1) ? (carousel_slides[carousel_slides.length-1] + carousel_slides[num] + carousel_slides[0]) : (carousel_slides[num-1] + carousel_slides[num] + carousel_slides[num+1]); //+carousel_slides[1];
     }
 } 
 ,300);
