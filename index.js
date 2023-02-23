fc5f4eb57faa45939bb13548695513be5668bc43
Oleksii-GitRepere <oleksii485th@gmail.com>
2023-02-23 22:34:59 +0200

    add btn next and btn prev in html

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var carousel_wrapper = document.getElementById("img_carousel_div");
+var carousel_wrapper = document.getElementById("img_carousel_prev_btn");
 /*
 var slide_2 = document.getElementById("slide_2");
 var slide_3 = document.getElementById("slide_3");
@@ -18,19 +18,18 @@ var carousel_slides = [`<div style="height: 320px; width: 100%"><img style="heig
     slide_5.innerHTML += `<img style="height: 100%; width: 100%" src="../img/desserts/Turkey/sobiyet.jpg" alt=""></img>`;
 */
 
-carousel_wrapper.innerHTML += `<button id="img_carousel_prev_btn" style="height: 30px; width: 60px">prev</button>`;
-
+// carousel_wrapper.innerHTML += `<button id="img_carousel_prev_btn" style="height: 30px; width: 60px">prev</button>`;
+// carousel_wrapper.innerHTML += `<button id="img_carousel_next_btn" style="height: 30px; width: 60px">next</button>`;
 
 var num=0;
 document.getElementById("img_carousel_next_btn").addEventListener("click",() =>
 {
-    num++;
+    // num++;
 });
 
 setInterval(()=>{
         document.getElementById("div_p").innerHTML = num;
         num = num>carousel_slides.length-1 ? 0 : num;
-        carousel_wrapper.innerHTML +=  carousel_slides[num]; //+carousel_slides[1];
+        carousel_wrapper.innerHTML =  carousel_slides[num]; //+carousel_slides[1];
     } 
 ,300);
-carousel_wrapper.innerHTML += `<button id="img_carousel_next_btn" style="height: 30px; width: 60px">next</button>`;
