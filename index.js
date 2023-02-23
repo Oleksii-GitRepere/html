e8acc0870f5e7440f3402010bd360261ec90898f
Oleksii-GitRepere <oleksii485th@gmail.com>
2023-02-23 22:36:49 +0200

    try get interval event

--- index.js
+++ index.js
@@ -21,15 +21,15 @@ var carousel_slides = [`<div style="height: 320px; width: 100%"><img style="heig
 // carousel_wrapper.innerHTML += `<button id="img_carousel_prev_btn" style="height: 30px; width: 60px">prev</button>`;
 // carousel_wrapper.innerHTML += `<button id="img_carousel_next_btn" style="height: 30px; width: 60px">next</button>`;
 
-// var num=0;
-// document.getElementById("img_carousel_next_btn").addEventListener("click",() =>
-// {
-//     // num++;
-// });
+var num=0;
+document.getElementById("img_carousel_next_btn").addEventListener("click",() =>
+{
+     num++;
+});
 
-// setInterval(()=>{
-//         document.getElementById("div_p").innerHTML = num;
+setInterval(()=>{
+        document.getElementById("div_p").innerHTML = num;
 //         num = num>carousel_slides.length-1 ? 0 : num;
 //         carousel_wrapper.innerHTML =  carousel_slides[num]; //+carousel_slides[1];
-//     } 
-// ,300);
+    } 
+,300);
