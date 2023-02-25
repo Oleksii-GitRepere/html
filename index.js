aeb1286bcb05209f3f7196160340c8ffe616ff1b
Oleksii-GitRepere <oleksii485th@gmail.com>
2023-02-25 21:39:31 +0200

    try create modal window

--- index.js
+++ index.js
@@ -59,3 +59,36 @@ setInterval(()=>
     }
 } 
 ,300);
+
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const openModalBtn = document.querySelector(".btn-open");
+const closeModalBtn = document.querySelector(".btn-close");
+
+
+const openModal = function () {
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+  };
+
+
+  openModalBtn.addEventListener("click", openModal);
+
+  const closeModal = function () {
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+  };
+
+  closeModalBtn.addEventListener("click", closeModal);
+
+
+  overlay.addEventListener("click", closeModal);
+
+  document.addEventListener("keydown");
+
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+      modalClose();
+    }
+  });
