38f888efb67709c8ed811d21536afd9ed4d15b8f
Oleksii-GitRepere <oleksii485th@gmail.com>
2023-02-25 11:29:55 +0200

    try create proper slides order

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ setInterval(()=>
 
         document.getElementById("useful_p").innerHTML = carousel_slides.length-num + " : " + num;
         // carousel_wrapper.innerHTML =  carousel_slides[] + carousel_slides[] + carousel_slides[];//(num+1 > carousel_slides.length-1) ? (carousel_slides[num] + carousel_slides[num] + carousel_slides[0]) : (carousel_slides[num] + carousel_slides[num+1] + carousel_slides[num+2]); //+carousel_slides[1];
-        for(let i=0; i<carousel_slides.length; i++)
+        for(let i=0; i<carousel_slides.length-1; i++)
         {
             wrap += carousel_slides[num1>length-1 ? 0 : num1++];
         }
