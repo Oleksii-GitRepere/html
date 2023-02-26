f8000bf9e63649cfa027a219a856d7f71b806fb5
Oleksii-GitRepere <oleksii485th@gmail.com>
2023-02-26 10:08:14 +0200

    img btn

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ const closeModalBtn = document.querySelector(".btn-close");
 
 
 
-document.getElementsByClassName("btn-open")
+document.getElementsById("btn")
         .addEventListener('click', function (event) {
             alert('Hi!');
         });
